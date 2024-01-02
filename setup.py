8b90abe1729cc32c58485e3a6f880caf25f94876
debugtitan <alts.devs@gmail.com>
2024-01-02 14:45:59 +0000

    new update

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 from setuptools import setup
 
-
 description = (
-    'Squad API Python '
+    'Python API Wrapper for Squadco.com Payment services'
 )
 
 setup(
     name='squadco',
     version='1.0.0',
-    author='Uche David <debugtitan.hub@outlook.com>, Joseph Folayan <folayanjoey@gmail.com>, Peter Adetunji <peteradetunji30@gmail.com>',
-    author_email='debugtitan.hub@outlook.com',
+    author='Uche David, Joseph Folayan, Peter Adetunji',
+    author_email='debugtitan.hub@outlook.com, folayanjoey@gmail.com, peteradetunji30@gmail.com',
     description=description,
-    long_description="Python Wrapper for Squadco.com Payment services",
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     license='MIT',
-    url='https://github.com/debugtitan/django-git-storage',
+    url='https://github.com/joey1123455/squad-py.git',
     keywords=['squadco payment gateway','squadco python package', 'squad'],
     packages=['squad'],
+    install_requires=['requests'],
     include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        "Programming Language :: Python :: 2",
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
 )
